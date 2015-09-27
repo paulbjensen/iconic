51a95babff0d7e16e569b483b30849ab7bbbfdbd
Paul Jensen <paulbjensen@gmail.com>
2015-09-27 17:39:23 +0100

    Dragging and dropping a file into the area displays that image in the icon set area

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 'use strict';
 
+function stopDefaultEvent (event) {
+	event.preventDefault();
+	return false;	
+}
 
+window.ondragover = stopDefaultEvent;
+window.ondrop = stopDefaultEvent;
 
 function displayImageInIconSet (filePath) {
 	var images = window.document.querySelectorAll('#icons img');
@@ -9,8 +15,27 @@ function displayImageInIconSet (filePath) {
 	}
 }
 
+function displayIconsSet () {
+	var iconsArea = window.document.querySelector('#icons');
+	iconsArea.style.display = 'block';
+}
 
+function interceptDroppedFile () {
+	var interceptArea = window.document.querySelector('#load-icon-holder');
+	interceptArea.ondrop = function (event) {
+		event.preventDefault();
+		if (event.dataTransfer.files.length !== 1) {
+			window.alert('You have dragged too many files into the app. Drag just 1 file');
+		} else {
+			interceptArea.style.display = 'none';
+			displayIconsSet();
+			var file = event.dataTransfer.files[0];
+			displayImageInIconSet(file.path);
+		}
+		return false;
+	};
+}
 
 window.onload = function () {
-
-}	
+	interceptDroppedFile();
+};
